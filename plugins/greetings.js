9c132698e7ce93926fbbe69302aebdbaea5cd383
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-10-05 10:38:53 +0530

    Update greetings.js

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -1,14 +1,13 @@
 /* Copyright (C) 2020 Yusuf Usta.
-
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const sql = require('./sql/greetings');
+const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('greetings');
