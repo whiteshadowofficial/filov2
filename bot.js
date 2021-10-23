8e6dec304a443a7562d731065c419bf81c4c1020
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-10-23 13:10:46 +0530

    ty

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.blueBright.italic(F'ilo  working perfectly 👻')
+            chalk.blueBright.italic('Filo  working perfectly 👻')
         );
     });
     
