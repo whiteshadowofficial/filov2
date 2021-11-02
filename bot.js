f3c601e08db831d1ca57569c7ec34641877fb774
Aflahxrd <88531428+nexusNw@users.noreply.github.com>
2021-11-02 12:42:35 +0530

    ```update```

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ async function whatsAsena () {
     
     
     const conn = new WAConnection();
-    conn.version = [3,3234,9];
+    conn.version = [2,2142,12];
     const Session = new StringSession();
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
